6d959ee9ebd315c4102bec6beffccbc32a7a7f0c
hickma <hickmat108@gmail.com>
2022-03-14 06:39:51 -0700

    added some data

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let todoArray = new Set();
 let user;
 
 //function to add new todo item
-const addTodo = () => {
-  if (todoArray.size != 5) {
+const addTodo = () =>{
+  if (todoArray.size != 5){
     if (todoInput.value != "") {
       todoInput.parentElement.classList.remove("error");
       todoArray.add(todoInput.value);
@@ -27,6 +27,7 @@ const addTodo = () => {
     todoInput.value = "";
   } else {
     alert("you have five things to do");
+
   }
 };
 //event handlers
@@ -55,3 +56,49 @@ const setTodo = (arr) => {
   todoList.innerHTML = htmlArr;
   // informText()}
 }
+
+
+
+//sign in functions
+const signIn = document.querySelector(".logo-wrap p")
+const modal = document.querySelector("#modal")
+const userNm = document.querySelector("input")
+const signBtn = document.querySelector("button")
+const disModal = document.querySelector("span")
+
+signIn.addEventListener("click", () =>{
+  modal.style.display = "flex"
+})
+disModal.addEventListener("click", (e) =>{
+  modal.style.display = "none"
+})
+modal.addEventListener("click", (e) =>{
+  e.target==modal? modal.style.display ="none" : null
+})
+//function to log in user and save todo list
+signBtn.addEventListener("click", () =>{
+  user = userNm.value
+  if (user ==""){
+    userNm.classList.add("error")
+  }else{
+    userNm.classList.remove("error")
+    signIn.innerText = user   //changing the "sign in" text to username
+    todoArray.clear()
+    setTodo(todoArray)
+    userNm.value = ""
+
+    let db = [...todoArray]
+    let _db =(JSON.stringify(db))
+    let userData = localStorage.getItem(user)
+
+    if(userData){
+      let _userData =JSON.parse(userData)
+      todoArray = new set(_userData)
+      setTodo(todoArray)
+      modal.style.display ="none"
+    }else{
+      localStorage.setItem(user, _db)
+      modal.style.display = "none"
+    }
+  }
+})
